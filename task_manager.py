b0906c42651a716aee8b378b64438c0919cad30b
Drew <drew.pickett@comcast.net>
2025-10-12 16:33:08 -0700

    corrected the index delete error

--- task_manager.py
+++ task_manager.py
@@ -25,6 +25,6 @@ class TaskManager:
 
     def delete_task(self, index):
         if self.tasks:
-            self.tasks.pop(index - 1)
+            self.tasks.pop(index)
             self.save_tasks()
 
